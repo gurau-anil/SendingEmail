4dc2739ad48555cf443139f2cb736fc980ee6017
Anil Gurau <gurau.anil@gmail.com>
2021-01-01 11:10:24 +0545

    validating whether email input is empty

--- SendingEmail/Program.cs
+++ SendingEmail/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SendingEmail
@@ -18,16 +19,44 @@ namespace SendingEmail
                 Console.WriteLine("Receiver's Email :");
                 mail.To = Console.ReadLine();
 
+
                 Console.WriteLine("Subject :");
                 mail.Subject = Console.ReadLine();
 
                 Console.WriteLine("Body :");
                 mail.Body = Console.ReadLine();
 
+                if(String.IsNullOrWhiteSpace(mail.To))
+                {
+                    Console.WriteLine("You must enter an email address.");
+                    continue;
+                }
+                else
+                {
+                    EmailSender mailSender = new EmailSender();
+                    mailSender.SendEmail(mail);
+                }
 
-                EmailSender mailSender = new EmailSender();
-                mailSender.SendEmail(mail);
-
+                Console.WriteLine("Want to send Email Again?");
+                Console.WriteLine("Press 'Y' to yes and 'N' to no.");
+                int choice=Console.Read();
+                if(choice == (int)'y' || choice == (int)'Y')
+                {
+                    continue;
+                }
+                else if(choice == (int)'n' || choice == (int)'N')
+                {
+                    Console.WriteLine("Exiting the program....");
+                    Thread.Sleep(3000);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong choice.");
+                    Console.WriteLine("Exiting the program....");
+                    Thread.Sleep(2000);
+                    Environment.Exit(-1);
+                }
                 Console.ReadLine();
             }
         }
